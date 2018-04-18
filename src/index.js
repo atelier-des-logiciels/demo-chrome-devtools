dc978a5533491c006786d83dd47c785926469749
Guillaume ARM <trapcodien@gmail.com>
2018-04-18 13:09:01 +0200

    chore(package): install and configure material-ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom';
+import { Typography, Paper } from 'material-ui';
+
+const styles = {
+  root: {
+    paddingTop: 10,
+    margin: 'auto',
+    maxWidth: '800px',
+    textAlign: 'center',
+  },
+  paper: {
+    padding: 10,
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div style={styles.root}>
+        <Typography variant="display2" gutterBottom>
+          TodoApp
+        </Typography>
+        <Paper style={styles.paper}>
+          Hello World
+        </Paper>
+      </div>
     )
   }
 }
