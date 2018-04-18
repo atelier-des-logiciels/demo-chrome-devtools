adf8954c9edb9d8cf6706b3d021512657b3fbd8f
Guillaume ARM <trapcodien@gmail.com>
2018-04-18 13:17:07 +0200

    refactor: split App and render

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from 'react'
-import { render } from 'react-dom';
-import { Typography, Paper } from 'material-ui';
 
-const styles = {
-  root: {
-    paddingTop: 10,
-    margin: 'auto',
-    maxWidth: '800px',
-    textAlign: 'center',
-  },
-  paper: {
-    padding: 10,
-  },
-};
+import App from './App';
+import render from './render';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div style={styles.root}>
-        <Typography variant="display2" gutterBottom>
-          TodoApp
-        </Typography>
-        <Paper style={styles.paper}>
-          Hello World
-        </Paper>
-      </div>
-    )
-  }
-}
-
-const container = document.querySelector('#root-container');
-render(<App />, container);
+render(<App />);
