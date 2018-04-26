4bab8fd0b6d7cf6333543d53dae2294895bc5731
Guillaume ARM <trapcodien@gmail.com>
2018-04-26 12:30:55 +0200

    fix(e2e): replace server.stop() by server.close()

--- e2e/index.js
+++ e2e/index.js
@@ -74,13 +74,26 @@ const main = async (config, server, tests) => {
 
 
 (async () => {
+//   const [jsCoverage, cssCoverage] = await Promise.all([
+//   page.coverage.stopJSCoverage(),
+//   page.coverage.stopCSSCoverage(),
+// ]);
+// let totalBytes = 0;
+// let usedBytes = 0;
+// const coverage = [...jsCoverage, ...cssCoverage];
+// for (const entry of coverage) {
+//   totalBytes += entry.text.length;
+//   for (const range of entry.ranges)
+//     usedBytes += range.end - range.start - 1;
+// }
+// console.log(`Bytes used: ${usedBytes / totalBytes * 100}%`);
   const tests = require('./tests');
   const config = require('./configuration');
 
   const server = await log('start server', startServer(config));
 
   await main(config, server, tests).catch(e => {
-    server.stop();
+    server.close();
     exitError(e);
   });
 })();
