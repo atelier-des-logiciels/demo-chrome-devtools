cdfb2c75aab2ec76c5ee83b095abf1a7eda7cb88
Guillaume ARM <trapcodien@gmail.com>
2018-04-27 18:51:55 +0200

    test(e2e): write check_all test

---
+++ e2e/tests/check_all.js
@@ -0,0 +1,22 @@
+const { evolve, not } = require('ramda');
+const assert = require('assert').strict;
+
+const initialTodoList = require('../../src/initialTodoList.json');
+const { getTodoListFromPage } = require('../utils');
+
+const inverseDone = evolve({ done: not });
+
+const expectedTodolist = initialTodoList.map(inverseDone);
+
+module.exports = async ({ page }) => {
+  const todoElements = await page.$$('#todo');
+  for (const todoElem of todoElements) {
+    const input = await todoElem.$('input');
+    await input.click()
+  }
+
+  const todolist = await getTodoListFromPage(page);
+  assert.deepEqual(todolist, expectedTodolist, 'All todos checkbox should be clicked');
+
+  await page.reload();
+};
