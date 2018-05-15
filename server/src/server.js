645e47e0dc498839cc2d533632a4ee8cb166257f
popul <paul@musso.io>
2018-05-15 02:48:36 +0200

    feat: Filter todos and transform them on server

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,13 @@ const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
+const R = require('ramda');
+
+const getFilter = R.cond([
+  [R.equals('active'), () => todo => !todo.done],
+  [R.equals('completed'), () => R.prop('done')],
+  [R.T, () => R.identity],
+]);
 
 const createRouter = (dataPath) => {
   const todoList = require(path.join(path.resolve(dataPath), 'todolist.json'));
@@ -13,12 +20,22 @@ const createRouter = (dataPath) => {
   router.get('/todos', (ctx) => {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.type= 'JSON';
+    
+    const timeStart = Date.now();
+    const list = R.pipe(
+      R.filter(getFilter(ctx.query.filter)),
+      R.sortBy(R.prop('date')),
+      R.map(R.evolve({
+        date: d => new Date(d),
+        value: R.replace(/^./, R.toUpper)
+      })),
+      R.take(20),
+    )(todoList);
 
-    if (ctx.query.filter !== 'all') {
-      throw new Error('filter not supported');
-    }
+    const dur = Date.now() - timeStart;
+    ctx.set('Server-Timing', `cpu;dur=${dur};desc="get todos"`);
 
-    ctx.body = JSON.stringify(todoList, null, 2);
+    ctx.body = JSON.stringify(list, null, 2);
   });
 
   return router;
